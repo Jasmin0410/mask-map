e34a19bca1ef3df4de61a63f46eacad212cd9e06
Jasmin Huang <wow92716@gmail.com>
2020-02-14 00:04:54 +0800

    Updates

---
+++ precache-manifest.cee93a2c3231e73b7758b7d696a2d9ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7d24afbb12e622e8d01eab1e86026c",
+    "url": "/mask-map/index.html"
+  },
+  {
+    "revision": "8f6ee35c97b79e342227",
+    "url": "/mask-map/static/css/main.cab0a9a0.chunk.css"
+  },
+  {
+    "revision": "97a5ea9f764027cdaa0e",
+    "url": "/mask-map/static/js/2.5123a6b4.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "/mask-map/static/js/2.5123a6b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6ee35c97b79e342227",
+    "url": "/mask-map/static/js/main.42002564.chunk.js"
+  },
+  {
+    "revision": "e102b1445583f8bd620b",
+    "url": "/mask-map/static/js/runtime-main.80cace50.js"
+  }
+]);
