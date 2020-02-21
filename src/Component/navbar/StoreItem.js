c834db03d3bf43880ed7ca2546a4dbfce5550d94
Jasmin Huang <wow92716@gmail.com>
2020-02-21 23:09:41 +0800

    Add icon

--- src/Component/navbar/StoreItem.js
+++ src/Component/navbar/StoreItem.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { updateLocation } from '../../redux/action';
 
+//import svg icon
+import marker from '../../style/img/marker.svg'
+import phone from '../../style/img/phone.svg'
+
 class StoreItem extends Component {
   constructor(props) {
     super(props);
@@ -9,7 +13,7 @@ class StoreItem extends Component {
 
     }
   }
-  
+
   render() {
     const storeInfo = this.props.store.properties;
     const storeLocation = this.props.store.geometry.coordinates;
@@ -24,8 +28,12 @@ class StoreItem extends Component {
         onClick={() => this.props.updateLocation([storeLocation[1], storeLocation[0]])}
       >
         <div className="store-name">{storeInfo.name}</div>
-        <div className="store-info">{storeInfo.address}</div>
-        <div className="store-info">{storeInfo.phone}</div>
+        <div className="store-info">
+          <img className="icon" src={marker} />{storeInfo.address}
+        </div>
+        <div className="store-info">
+          <img className="icon" src={phone} />{storeInfo.phone}
+        </div>
         <div className="mask-status">
           <div className={`mask-item adult ${maskStatus(adult)}`}>大人: {adult}</div>
           <div className={`mask-item child ${maskStatus(child)}`}>小孩: {child} </div>
